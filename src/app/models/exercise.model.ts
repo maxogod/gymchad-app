9ed11fa58503ee23e1b81abab56d153eedfac691
maxogod <maxi18wars@gmail.com>
2023-08-21 23:51:39 -0300

    static activity page almost done

--- src/app/models/exercise.model.ts
+++ src/app/models/exercise.model.ts
@@ -1,1 +1,9 @@
-export default interface Exercise {}
+export default interface Exercise {
+    id: String;
+    name: String;
+    picture: String;
+    description: String;
+    sets: Number;
+    reps: Number;
+    time: Number;
+}
--- src/app/modules/activity/activity.component.ts
+++ src/app/modules/activity/activity.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Exercise from 'src/app/models/exercise.model';
 
 @Component({
   selector: 'app-activity',
@@ -14,23 +15,31 @@ export class ActivityComponent {
     exercises: [
       {
         id: "64e3c2695122076a30ff01e5",
-        name: "ex1",
-        picture: "some pic",
+        name: "ex1 pposfds",
+        picture: "https://metro.co.uk/wp-content/uploads/2017/03/0c6021073bce714b365904efb2f1fb5b.jpg",
         description: "some descrip",
         sets: 1,
         reps: 2,
-        time: 3
+        time: 0
       },
       {
         id: "64e3c2695122076a30ff01e5",
-        name: "ex1",
-        picture: "some pic",
-        description: "some descrip",
-        sets: 1,
-        reps: 2,
+        name: "ex1fdfd 00ss",
+        picture: "https://cdn.vox-cdn.com/thumbor/1tPy-AFZtQwS8O2EfSWnUS2vHjM=/138x0:2361x1667/1200x800/filters:focal(138x0:2361x1667)/cdn.vox-cdn.com/uploads/chorus_image/image/55908333/1_KzqzL4Fz0a224QKJn0aY4w.0.jpeg",
+        description: "some veeeery lonnggg descriptionnnnnnnnn",
+        sets: 0,
+        reps: 0,
         time: 3
       }
     ]
+  }
+
+  openedExercises: boolean[] = Array(this.activity.exercises.length).fill(false);
+
+  constructor() { }
+
+  toggleDetails(index: number) {
+    this.openedExercises[index] = !this.openedExercises[index];
   }
 
 }
